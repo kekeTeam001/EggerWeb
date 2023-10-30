2a8fee01cd93f6e33d8e72d1b98b18a8a1dcd20c
kekekuli <kekekuliobj@gmail.com>
2023-10-30 10:12:08 +0800

    重新配置tailwind css使用tw-前缀

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
+  prefix: 'tw-',
 }
-
